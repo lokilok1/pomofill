6372ffbec94d83c075b6bfc0e8874f7e113a898e
samchan0221 <samchan0221@gmail.com>
2023-06-01 10:30:26 +0800

    Remove explicit currying of eventHandler

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -7,31 +7,22 @@ const init = () => {
   const homeStore = new HomeStore();
   const timerView = new TimerView();
   timerView.render(homeStore.counter);
-  timerView.addHandlerTimerBtns(
-    timerView.startButton,
-    homeStore.start.bind(homeStore)
-  );
-  timerView.addHandlerTimerBtns(
-    timerView.stopButton,
-    homeStore.stop.bind(homeStore)
-  );
-  timerView.addHandlerTimerBtns(
-    timerView.resetButton,
-    homeStore.reset.bind(homeStore)
-  );
+  timerView.addHandlerTimerBtns(timerView.startButton, () => homeStore.start());
+  timerView.addHandlerTimerBtns(timerView.stopButton, () => homeStore.stop());
+  timerView.addHandlerTimerBtns(timerView.resetButton, () => homeStore.reset());
   timerView.addHandlerTimerBtns(
     timerView.pomodoroSessionButton,
-    () => homeStore.changeDuration(25, 0) // set to 25 min for pomodoro session
+    () => homeStore.changeDuration(25, 0), // set to 25 min for pomodoro session
   );
 
   timerView.addHandlerTimerBtns(
     timerView.longBreakButton,
-    () => homeStore.changeDuration(10, 0) // set to 10 min for long break
+    () => homeStore.changeDuration(10, 0), // set to 10 min for long break
   );
 
   timerView.addHandlerTimerBtns(
     timerView.shortBreakButton,
-    () => homeStore.changeDuration(5, 0) // set to 5 min for short break
+    () => homeStore.changeDuration(5, 0), // set to 5 min for short break
   );
   homeStore.addEventListener("save", () => {
     timerView.render(homeStore.counter);
