210625db4c77049ebbd450abab705421b225a57e
samchan0221 <samchan0221@gmail.com>
2023-06-01 11:07:28 +0800

    Use private class features

--- src/js/home/models.js
+++ src/js/home/models.js
@@ -1,69 +1,66 @@
 import { defaultConfig } from "../shared/config.js";
 
 export default class HomeStore extends EventTarget {
+  #timerId = null;
+  #config;
+  #currentMode;
+  counter;
   constructor() {
     super();
-    this.timerId = null;
-    this.currentMode = "pomodoro";
-    // TODO: try to fetch config from local storage first
-    this.config = defaultConfig;
+    this.#config = defaultConfig;
+    this.#currentMode = "pomodoro"; // TODO: try to fetch config from local storage first
     this.reset();
   }
 
-  _getInitialDuration() {
-    switch (this.currentMode) {
+  #getInitialDuration() {
+    switch (this.#currentMode) {
       case "pomodoro":
-        return this.config.pomodoroDuration;
+        return this.#config.pomodoroDuration;
       case "short-break":
-        return this.config.shortBreakDuration;
+        return this.#config.shortBreakDuration;
       case "long-break":
-        return this.config.longBreakDuration;
+        return this.#config.longBreakDuration;
       default:
         throw new Error("invalid timer modd");
     }
   }
 
-  _save() {
+  #save() {
     this.dispatchEvent(new CustomEvent("save"));
   }
 
-  configure(options) {
-    this.initialDuration = options.duration;
-    this.reset();
-  }
-
   start() {
-    if (this.timerId != null) {
-      clearInterval(this.timerId);
+    if (this.#timerId != null) {
+      clearInterval(this.#timerId);
     }
-    this.timerId = setInterval(() => {
+    this.#timerId = setInterval(() => {
       if (this.counter > 0) {
         this.counter--;
       } else {
         this.stop();
       }
-      this._save();
+      this.#save();
     }, 1000);
   }
 
   stop() {
-    if (this.timerId) {
-      clearInterval(this.timerId);
-      this.timerId = null;
+    if (this.#timerId) {
+      clearInterval(this.#timerId);
+      this.#timerId = null;
     }
   }
 
   reset() {
     this.stop();
-    this.counter = this._getInitialDuration();
-    this._save();
+    this.counter = this.#getInitialDuration();
+    this.#save();
   }
 
   setMode(mode) {
-    if (this.currentMode == mode) {
+    if (this.#currentMode == mode) {
       return;
     }
-    this.currentMode = mode;
+    this.#currentMode = mode;
     this.reset();
   }
 }
